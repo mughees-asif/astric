33c16702e4ea55fea876f16665f32bf1283cfb85
mughees-asif <mughees460@gmail.com>
2020-04-14 20:48:59 +0100

    onboarding

--- constants/Images.js
+++ constants/Images.js
@@ -1,4 +1,4 @@
-const Onboarding = 'https://images.unsplash.com/photo-1505995433366-e12047f3f144?fit=crop&w=840&q=80';
+const Onboarding = 'https://images.unsplash.com/photo-1527067829737-402993088e6b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80';
 const Pro = 'https://images.unsplash.com/photo-1485796826113-174aa68fd81b?fit=crop&w=840&q=80';
 const Products = {
   'Accessories': 'https://source.unsplash.com//l1MCA0VyNrk/840x840',
